fe1717094aaf008c322a37250ebd2e2f3b878067
Juan David <barbosaneira@gmail.com>
2026-03-25 11:17:33 -0500

    Validation Express Validator

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,20 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { ProjectController } from "../controllers/ProjectController";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
 
-router.post('/', ProjectController.createProject )
+router.post('/',
+    body('projectName')
+       .notEmpty().withMessage('El Nombre del Proyecto es requerido'),
+    body('clientName')
+       .notEmpty().withMessage('El Nombre del Cliente es requerido'),
+    body('description')
+       .notEmpty().withMessage('La Descripción del Proyecto es requerida'),
+     handleInputErrors,
+     ProjectController.createProject 
+      )
 router.get('/', ProjectController.getAllProjects)
 
 export default router;
